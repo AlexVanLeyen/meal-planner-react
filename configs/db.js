3c08fbcc4c8df13eefbf48c36a8098b726b94a18
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-07-25 22:54:58 +0800

    feat: Added winston logging

--- configs/db.js
+++ configs/db.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 const ConnectionError = require("../errors/ConnectionError");
+const logger = require("./logger");
 
 module.exports = function ({ url }) {
     if (!url) {
         throw new ConnectionError("DB_URL is not defined.");
     }
+
+    mongoose.connection.on('error', error => logger.error(error));
+    mongoose.connection.on('disconnected', () => logger.error('Unexpected disconnect from database.'));
+
     return mongoose.connect(url);
 }
