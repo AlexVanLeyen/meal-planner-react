abf4d3718c59c360910b36c81f68ddf13455bfd6
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-07-31 01:48:37 +0800

    feat: Added theme switcher component

---
+++ client/src/common/components/ThemeSwitcher/index.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from 'react';
+
+export interface ThemeSwitcher {
+    themes?: string[]
+}
+
+export const ThemeSwitcher: React.FC<ThemeSwitcher> = props => {
+    const { themes = [] } = props;
+
+    const handleThemeChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
+        if (document.firstElementChild) {
+            document.firstElementChild.classList.value = event.target.value;
+        }
+    }, [])
+
+    return (
+        <div className="theme-switcher">
+            <label>
+                <div>Theme:</div>
+                <div className="select">
+                    <select onChange={handleThemeChange}>
+                        { themes.map( theme => (
+                            <option key={theme} value={`theme-${theme}`}>{theme.replaceAll("-", " ")}</option>
+                        )) }
+                        { themes.length == 0 && (
+                            <option value="" disabled>No Themes Available</option>
+                        ) }
+                    </select>
+                </div>
+            </label>
+        </div>
+    );
+}
