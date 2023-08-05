33077db7769d831ae115a97f1ef6788489035734
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-08-05 12:44:32 +0800

    chore: Enabled path aliasing

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -45,7 +45,12 @@ const config: Configuration = {
         ]
     },
     resolve: {
-        extensions: [".tsx", ".ts", ".js"]
+        extensions: [".tsx", ".ts", ".js"],
+        alias: {
+            "@": [
+                path.resolve(__dirname, 'src')
+            ]
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
