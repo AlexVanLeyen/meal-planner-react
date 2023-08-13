9cc7d8ccc76c7834956cbbca26a1b4dd92eaf317
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-08-13 21:46:12 +0800

    feat: Added Navigation and main layout

---
+++ client/src/layouts/MainLayout/index.tsx
@@ -0,0 +1,19 @@
+import Navigation from '@/navigation/components/Navigation';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout: React.FC = () => {
+
+    return (
+        <>
+            <div id="navigation">
+                <Navigation />
+            </div>
+            <main id="main-content">
+                <Outlet />
+            </main>
+        </>
+    );
+};
+
+export default MainLayout;
