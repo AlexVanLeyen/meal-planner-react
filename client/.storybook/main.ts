640860f09871d6a943652603a3d9dbc7649cf478
AlexVanLeyen <alex.vanleyen@gmail.com>
2023-10-22 11:49:45 +0800

    chore: Added storybook

---
+++ client/.storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+import path from 'path';
+
+const config: StorybookConfig = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-interactions",
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    async webpackFinal (config) {
+        if (config?.resolve?.alias) {
+            config.resolve.alias["@"] = path.resolve(__dirname, '../src')
+        }
+        return config;
+    }
+};
+export default config;
