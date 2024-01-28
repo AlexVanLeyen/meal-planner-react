9131e192fbb126e93b17e0cf9157552940bf29ec
AlexVanLeyen <alex.vanleyen@gmail.com>
2024-01-28 17:14:18 +0800

    fix: process is not defined

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,4 +1,4 @@
-export const BASE = process?.env?.REACT_APP_API_URL ?? "";
+export const BASE = window.process?.env?.REACT_APP_API_URL ?? "";
 export const MEAL_PLAN = `${BASE}/plans`;
 export const MEAL_PLANS = `${BASE}/plans`;
 
